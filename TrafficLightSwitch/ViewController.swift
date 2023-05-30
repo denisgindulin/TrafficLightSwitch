dba43ae498676e53858434e3f34c36b5bbcd9bae
Denis Gindulin <denisgindulin@me.com>
2023-05-30 16:00:50 +0300

    setup start state of UI components

--- TrafficLightSwitch/ViewController.swift
+++ TrafficLightSwitch/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+
+        switchButton.layer.cornerRadius = 10
     }
-
-
+    
 }
 
