a048473b385005c86162f7b392a276f7643db232
Denis Gindulin <denisgindulin@me.com>
2023-05-30 12:15:00 +0300

    Initial Commit

---
+++ TrafficLightSwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightSwitch
+//
+//  Created by Денис Гиндулин on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
